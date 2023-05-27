fa9de43dda8c884cd4ffe996e064978ee0f3b256
luziferus <luzy-dev@pm.me>
2023-05-27 11:30:24 +0200

    :sparkles: added some images

--- scripts/build_caprover_static_page.sh
+++ scripts/build_caprover_static_page.sh
@@ -5,5 +5,6 @@ mkdir -p ../dist/static/download &&
 cp ../static/Dockerfile ../dist/Dockerfile &&
 cp ../static/captain-definition ../dist/captain-definition &&
 cp ../static/index.html ../dist/static/index.html &&
+cp -r ../static/public ../dist/static/public &&
 zip -r ../dist/static/download/morgenrot.zip ../source/.
 tar -czf ../caprover.tar -C ../dist .
